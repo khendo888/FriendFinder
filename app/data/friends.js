b749083eb4acb5cb92e779b76b99d58c37ba32b4
khendo888 <khendo888@gmail.com>
2017-08-29 03:53:20 -0400

    updates

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+//holds all of data for all of the friends
+var friends = [
+	{
+		name:"Idris Elba"
+		picture: "http://mediamass.net/jdd/public/documents/celebrities/3312.jpg"
+		scores:[3,3,3,3,3,3,3,3,3,3]
+	},
+	{
+		name:"Derek Luke"
+		picture: "http://wikipicky.com/uploads/celebrity/Derek%20Luke.jpg"
+		scores:[3,2,3,4,3,5,3,6,3,3]
+	}
+]
+
+module.exports = friends
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,7 @@
+var friends = require("../data/friends.js");
+
+module.exports=function(app){
+	app.get("/api/friends",function(req,res){
+		res.json(friends);
+}
+//how to match users with scores; calculate difference between friends to find best match
