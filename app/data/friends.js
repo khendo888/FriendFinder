74a9a8ba1699a600a72b6ad9c412fdc967b6eac8
khendo888 <khendo888@gmail.com>
2017-08-30 20:34:27 -0400

    additions and corrections

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,13 @@
 //holds all of data for all of the friends
 var friends = [
 	{
-		name:"Idris Elba"
-		picture: "http://mediamass.net/jdd/public/documents/celebrities/3312.jpg"
+		name:"Idris Elba",
+		picture: "http://mediamass.net/jdd/public/documents/celebrities/3312.jpg",
 		scores:[3,3,3,3,3,3,3,3,3,3]
 	},
 	{
-		name:"Derek Luke"
-		picture: "http://wikipicky.com/uploads/celebrity/Derek%20Luke.jpg"
+		name:"Derek Luke",
+		picture: "http://wikipicky.com/uploads/celebrity/Derek%20Luke.jpg",
 		scores:[3,2,3,4,3,5,3,6,3,3]
 	}
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,5 +3,6 @@ var friends = require("../data/friends.js");
 module.exports=function(app){
 	app.get("/api/friends",function(req,res){
 		res.json(friends);
-}
+	});
+};
 //how to match users with scores; calculate difference between friends to find best match
